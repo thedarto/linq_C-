a9fc437ae573cfbab3544f4b5e02104859a7b786
Darwin <dartap31@hotmail.com>
2023-11-30 22:32:23 -0500

    Operadores básicos

---
+++ LinqQueries.cs
@@ -0,0 +1,95 @@
+public class LinqQueries
+{
+    private List<Book> librosCollection=new  List<Book>();
+    public LinqQueries ()
+    {
+        using(StreamReader reader=new StreamReader("books.json"))
+        {
+            string json= reader.ReadToEnd();
+            this.librosCollection=System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions(){PropertyNameCaseInsensitive=true});
+        }
+    }
+    public IEnumerable<Book>TodaLaColeccion()
+    {
+        return librosCollection;
+    }
+    //***OPERADORES BÄSICOS
+    //operador where para realizar búsquedas
+    public IEnumerable <Book> LibrosDespuesdel2000()
+    {
+        //extension method
+        //return librosCollection.Where(p=>p.PublishedDate.Year>2000);
+
+        //query expresion
+        return from l in librosCollection where l.PublishedDate.Year >2000 select l;
+    }
+    //Where filtra datos de una coleccion
+    public IEnumerable <Book> LibrosConMasde250PagConPalabrasinAction()
+    {
+        //extension methods
+        return librosCollection.Where(p =>p.PageCount>250 && p.Title.Contains("in Action") );
+
+        //query expresions
+        return from l in librosCollection where l.PageCount > 250 && l.Title.Contains("in action") select l;
+    }
+    //all y any devuelven un booleano; sirven para hacer comprobaciones en la colección
+    // all: debe cumplir la condicion para todos los elementos
+    public bool TodosLosLibrosTienenStatus()
+    {
+        return librosCollection.All(p=>p.Status!=string.Empty);
+    }
+    // any: si almenos uno cumple con la condición
+    public bool SiAlgunLibroFuePublicado2005()
+    {
+        return librosCollection.Any(p=>p.PublishedDate.Year==2005);
+    }
+    //contains: si existe o no un elemento especifico en la coleccion
+    //Where si cumple la condición retorna los valores 
+    public IEnumerable <Book> LibrosdePython()
+    {
+        return librosCollection.Where(p=>p.Categories.Contains("Python"));
+    }
+    //Ordenar: Operador OrderBy y OrderbyDescending
+    //OrderBy
+    public IEnumerable <Book> LibrosdeJavaPorNombreAscendente()
+    {
+        return librosCollection.Where(p=>p.Categories.Contains("Java")).OrderBy(p=> p.Title);
+    }
+    //OrderByDescending
+    public IEnumerable <Book> LibrosDeMasDe450PagOrdenadosPorNumPagDes()
+    {
+        return librosCollection.Where(p=>p.PageCount>450).OrderByDescending(p=> p.PageCount);
+    }
+    //Operador Take and Skip
+    //Tak: cantidad de elementos a tomar
+    //Skip: omitir cierta cantidad de registro y seleccionar el resto
+    //Take
+    public IEnumerable <Book> TresPrimerosLibrosOrdenadosPorFecha()
+    {
+        return librosCollection.Where(p=>p.Categories
+        .Contains("Java"))
+        .OrderByDescending(p=> p.PublishedDate)
+        .Take(3);
+    }
+    //Skip
+    public IEnumerable <Book> TerceryCuartoLibroDeMas400Pag()
+    {
+        return librosCollection
+        .Where(p=>p.PageCount>400)
+        .Take(4)
+        .Skip(2);
+    }
+    //Select: filtrar columnas
+    public IEnumerable <Book> TresPrimerosLibrosDeLaColeccion()
+    {
+        return librosCollection.Take(3)
+        .Select(p=>new Book () {Title=p.Title, PageCount= p.PageCount});
+    }
+//operadores de agregacion; permite realizar calculo sobre toda la operacion y devolver un dato en especifico
+//LongCount soporta 64 bits y Count: soporta 32 bits (realizan la cuenta de elementos) 
+    public long CantidadDeLibrosEntre200y500Pag()
+    {
+        //return librosCollection.Where(p=>p.PageCount>=200 && p.PageCount<=500).LongCount();
+        return librosCollection.LongCount(p=>p.PageCount>=200 && p.PageCount<=500);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+LinqQueries queries= new LinqQueries();
+//toda la colección
+//ImprimirValores(queries.TodaLaColeccion());
+
+//Libros despues del 2000
+//ImprimirValores(queries.LibrosDespuesdel2000());
+
+//Libros que tienen mas de 250 pag y tienen en el titulo la palabra in Action
+//ImprimirValores(queries.LibrosConMasde250PagConPalabrasinAction());
+
+//Todos los libros tienen status
+//Console.WriteLine($"todos los libros tienene status:-{queries.TodosLosLibrosTienenStatus()}");
+
+//Si algún libro fue publicado en 2005
+//Console.WriteLine($"Algún libro fue publicado en 2005?:-{queries.SiAlgunLibroFuePublicado2005()}");
+
+//Libros de Python
+//ImprimirValores(queries.LibrosdePython());
+
+//Libros de java ordenados por nombre
+//ImprimirValores(queries.LibrosdeJavaPorNombreAscendente());
+
+//Libros que tienen mas de 450 paginas ordenados por cantidad de paginas
+//ImprimirValores(queries.LibrosDeMasDe450PagOrdenadosPorNumPagDes());
+
+//tres libros de Java mas publicados recientemente
+//ImprimirValores(queries.TresPrimerosLibrosOrdenadosPorFecha());
+
+//tres libros de Java mas publicados recientemente
+//ImprimirValores(queries.TerceryCuartoLibroDeMas400Pag());
+
+//cantidad de libros que tienen entre 200 y 500 paginas
+//ImprimirValores(queries.TresPrimerosLibrosDeLaColeccion());
+
+//tres primeros libros filtrados con Select
+Console.WriteLine($"Cantidad de libros que tiene entre 200 y 500 pag:{queries.CantidadDeLibrosEntre200y500Pag()}");
+
+
+void ImprimirValores(IEnumerable<Book> listadelibros)
+{
+    Console.WriteLine("{0,-60} {1,15} {2,15}\n","Titulo","N paginas","Fecha publicacion");
+    foreach (var item in listadelibros)
+    {
+        Console.WriteLine("{0,-60} {1,15} {2,15}",item.Title,item.PageCount,item.PublishedDate.ToShortDateString());
+    }
+}
